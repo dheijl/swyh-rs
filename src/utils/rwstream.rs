2f66866fa22434680bb99ee65de32e76546f195e
dheijl <danny.heijl@telenet.be>
2021-02-18 15:58:33 +0100

    In theory this should work for WAV format

--- src/utils/rwstream.rs
+++ src/utils/rwstream.rs
@@ -10,6 +10,7 @@
 ///
 */
 use crossbeam_channel::{Receiver, Sender};
+use log::debug;
 use std::collections::VecDeque;
 use std::io::Read;
 use std::io::Result as IoResult;
@@ -65,7 +66,7 @@ impl ChannelStream {
         self.silence = Vec::with_capacity(size);
         self.silence.resize(size, 0i16);
         // Hack for Bubble with Chromecast/Nest
-        self.fifo.extend(self.silence.clone());
+        //self.fifo.extend(self.silence.clone());
     }
 
     pub fn write(&self, samples: &[i16]) {
@@ -117,28 +118,31 @@ impl Read for ChannelStream {
 }
 
 // create an "infinite size" wav hdr
+// note this may not work when streaming to a "libsndfile" based renderer
+// as libsndfile insists on a seekable WAV file depending on the open mode used
 fn create_wav_hdr(sample_rate: u32) -> Vec<u8> {
     let mut hdr = [0u8; 44];
     let channels: u16 = 2;
     let bits_per_sample: u16 = 16;
-    let samples_per_channel: u32 = sample_rate / channels as u32;
-    let byte_rate: u32 = sample_rate * channels as u32 * bits_per_sample as u32 / 8;
-    let block_align: u16 = channels * bits_per_sample / 8;
-    hdr[..4].copy_from_slice(b"RIFF"); //ChunkId
-    let chunksize = std::u32::MAX;
+    let bytes_per_sample: u16 = bits_per_sample / 8;
+    let block_align: u16 = channels * bytes_per_sample;
+    let byte_rate: u32 = sample_rate * block_align as u32;
+    hdr[0..4].copy_from_slice(b"RIFF"); //ChunkId
+    let subchunksize: u32 = std::u32::MAX; // "infinite" data chunksize signal value
+    let chunksize: u32 = subchunksize; // "infinite" RIFF chunksize signal value
     hdr[4..8].copy_from_slice(&chunksize.to_le_bytes()); // ChunkSize
-    hdr[8..12].copy_from_slice(b"WAVE"); // Format
-    hdr[12..16].copy_from_slice(b"fmt "); // Format
-    hdr[16..20].copy_from_slice(&16u32.to_le_bytes()); // SubChunk1Size PCM
-    hdr[20..22].copy_from_slice(&1u16.to_le_bytes()); // AudioFormat PCM
+    hdr[8..12].copy_from_slice(b"WAVE"); // File Format
+    hdr[12..16].copy_from_slice(b"fmt "); // SubChunk = Format
+    hdr[16..20].copy_from_slice(&16u32.to_le_bytes()); // SubChunk1Size for PCM
+    hdr[20..22].copy_from_slice(&1u16.to_le_bytes()); // AudioFormat: uncompressed PCM
     hdr[22..24].copy_from_slice(&channels.to_le_bytes()); // numchannels 2
-    hdr[24..28].copy_from_slice(&samples_per_channel.to_le_bytes()); // SampleRate
-    hdr[28..32].copy_from_slice(&byte_rate.to_le_bytes()); // ByteRate
+    hdr[24..28].copy_from_slice(&sample_rate.to_le_bytes()); // SampleRate
+    hdr[28..32].copy_from_slice(&byte_rate.to_le_bytes()); // ByteRate (Bps)
     hdr[32..34].copy_from_slice(&block_align.to_le_bytes()); // BlockAlign
     hdr[34..36].copy_from_slice(&bits_per_sample.to_le_bytes()); // BitsPerSample
     hdr[36..40].copy_from_slice(b"data"); // SubChunk2Id
-    hdr[40..44].copy_from_slice(&chunksize.to_le_bytes()); // SubChunk2Size
-    /*eprintln!("Header: {:02x?}", hdr);*/
+    hdr[40..44].copy_from_slice(&subchunksize.to_le_bytes()); // SubChunk2Size
+    debug!("WAV Header (l={}): \r\n{:02x?}", hdr.len(), hdr);
     hdr.to_vec()
 }
 

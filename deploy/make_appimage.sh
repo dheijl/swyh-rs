16d35708647dd77bec36521ee7fa31152c0e3490
dheijl <danny.heijl@telenet.be>
2022-10-16 13:24:08 +0200

    Update make_appimage.sh

--- deploy/make_appimage.sh
+++ deploy/make_appimage.sh
@@ -2,7 +2,7 @@
 
 if sudo cp ~/Documenten/GitHub/swyh-rs/target/release/swyh-rs /usr/bin/swyh-rs 
 then
-	[[ -f AppDir ]] && rm -r AppDir
+	[[ -d AppDir ]] && rm -r AppDir
 
 	./linuxdeploy-x86_64.AppImage -e /usr/bin/swyh-rs -d ~/Documenten/GitHub/swyh-rs/deploy/swyh-rs.desktop -i ~/Documenten/GitHub/swyh-rs/deploy/n256.png --appdir AppDir --output appimage
 

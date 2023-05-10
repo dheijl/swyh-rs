d25d0ea35dd5cebc3ec3a92144e6d46f904320e5
dheijl <danny.heijl@telenet.be>
2023-05-10 13:57:38 +0200

    cli appimage

--- deploy/make_appimage.sh
+++ deploy/make_appimage.sh
@@ -9,5 +9,13 @@ then
 	sudo rm /usr/bin/swyh-rs
 fi
 
+if sudo cp ~/Documenten/GitHub/swyh-rs/target/release/swyh-rs-cli /usr/bin/swyh-rs-cli 
+then
+	[[ -d AppDir ]] && rm -r AppDir
+
+	./linuxdeploy-x86_64.AppImage -e /usr/bin/swyh-rs-cli -d ~/Documenten/GitHub/swyh-rs/deploy/swyh-rs-cli.desktop -i ~/Documenten/GitHub/swyh-rs/deploy/n256.png --appdir AppDir --output appimage
+
+	sudo rm /usr/bin/swyh-rs-cli
+fi
 
 

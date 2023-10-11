592b4811b70a166b9ca4acb47f03af11a3095160
dheijl <danny.heijl@telenet.be>
2023-10-11 15:02:58 +0200

    correct some WAV header values

--- src/utils/rwstream.rs
+++ src/utils/rwstream.rs
@@ -190,20 +190,20 @@ fn create_wav_hdr(sample_rate: u32, bits_per_sample: u16) -> Vec<u8> {
     let block_align: u16 = channels * bytes_per_sample;
     let byte_rate: u32 = sample_rate * block_align as u32;
     hdr[0..4].copy_from_slice(b"RIFF"); //ChunkId, little endian WAV
-    let subchunksize: u32 = std::u32::MAX; // "infinite" data chunksize signal value
-    let chunksize: u32 = subchunksize; // "infinite" RIFF chunksize signal value
-    hdr[4..8].copy_from_slice(&chunksize.to_le_bytes()); // ChunkSize
+    let chunksize: u32 = 4294967286; // max RIFF chunksize
+    let subchunksize: u32 = 4294967248; // max data chunksize signal value
+    hdr[4..8].copy_from_slice(&chunksize.to_le_bytes()); // RIFF ChunkSize
     hdr[8..12].copy_from_slice(b"WAVE"); // File Format
     hdr[12..16].copy_from_slice(b"fmt "); // SubChunk = Format
-    hdr[16..20].copy_from_slice(&16u32.to_le_bytes()); // SubChunk1Size for PCM
+    hdr[16..20].copy_from_slice(&16u32.to_le_bytes()); // fmt chunksize for PCM
     hdr[20..22].copy_from_slice(&1u16.to_le_bytes()); // AudioFormat: uncompressed PCM
     hdr[22..24].copy_from_slice(&channels.to_le_bytes()); // numchannels 2
     hdr[24..28].copy_from_slice(&sample_rate.to_le_bytes()); // SampleRate
     hdr[28..32].copy_from_slice(&byte_rate.to_le_bytes()); // ByteRate (Bps)
     hdr[32..34].copy_from_slice(&block_align.to_le_bytes()); // BlockAlign
     hdr[34..36].copy_from_slice(&bits_per_sample.to_le_bytes()); // BitsPerSample
-    hdr[36..40].copy_from_slice(b"data"); // SubChunk2Id
-    hdr[40..44].copy_from_slice(&subchunksize.to_le_bytes()); // SubChunk2Size
+    hdr[36..40].copy_from_slice(b"data"); // SubChunk = "data"
+    hdr[40..44].copy_from_slice(&subchunksize.to_le_bytes()); // data SubChunkSize
     debug!("WAV Header (l={}): \r\n{:02x?}", hdr.len(), hdr);
     hdr.to_vec()
 }

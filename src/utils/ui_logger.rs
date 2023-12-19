1902ddc2fba1c5a62c2c617b6bd1b2cf70850af4
dheijl <danny.heijl@telenet.be>
2023-12-19 12:51:06 +0100

    simplify ui_log()

--- src/utils/ui_logger.rs
+++ src/utils/ui_logger.rs
@@ -5,8 +5,9 @@ use log::{error, info, warn};
 #[cfg(feature = "gui")]
 use crate::globals::statics::LOGCHANNEL;
 
-/// `ui_log` - send a logmessage to the textbox on the Crossbeam LOGCHANNEL
-#[cfg(feature = "gui")]
+/// `ui_log`
+/// - log a messgae to the terminal and the logfile
+/// - send a logmessage to the textbox on the Crossbeam LOGCHANNEL ig runing the GUI
 pub fn ui_log(s: &str) {
     let cat: &str = &s[..2];
     match cat {
@@ -14,18 +15,11 @@ pub fn ui_log(s: &str) {
         "*E" => error!("tb_log: {}", s),
         _ => info!("tb_log: {}", s),
     };
-    let logger = &LOGCHANNEL.read().0;
-    logger.send(s.to_string()).unwrap();
-    app::awake();
-    std::thread::yield_now();
-}
-
-#[cfg(feature = "cli")]
-pub fn ui_log(s: &str) {
-    let cat: &str = &s[..2];
-    match cat {
-        "*W" => warn!("{s}"),
-        "*E" => error!("{s}"),
-        _ => info!("{s}"),
-    };
+    #[cfg(feature = "gui")]
+    {
+        let logger = &LOGCHANNEL.read().0;
+        logger.send(s.to_string()).unwrap();
+        app::awake();
+        std::thread::yield_now();
+    }
 }

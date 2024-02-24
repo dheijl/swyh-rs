48ab2f540d7a62b9b583092430da832cb63d2bea
Yury Shvedov <yury.shvedov@kaspersky.com>
2024-02-24 09:40:02 +0300

    [local-ip] Use local_ip_address crate
    This module implement cross-platform detecting of local address without
    outgoing connections. This useful to run swyh when you do not any
    internet connection (e.g. test).
    
    Change-Id: I53dcd481bd03e8be45f6c388c09721090422cb91

--- src/utils/local_ip_address.rs
+++ src/utils/local_ip_address.rs
@@ -1,23 +1,11 @@
 use if_addrs::IfAddr;
-use std::net::{IpAddr, UdpSocket};
+use std::net::IpAddr;
+use local_ip_address::local_ip;
 
 /// `get_local_address` - get the local ip address, return an `Option<String>`. when it fails, return `None`.
 #[must_use]
-pub fn get_local_addr() -> Option<IpAddr> {
-    // bind to IN_ADDR_ANY, can be multiple interfaces/addresses
-    // try to connect to Google DNS so that we bind to an interface connected to the internet
-    let Ok(socket) = UdpSocket::bind("0.0.0.0:0") else {
-        return None;
-    };
-    match socket.connect("8.8.8.8:80") {
-        Ok(()) => (),
-        Err(_) => return None,
-    };
-    // now we can return the IP address of this interface
-    match socket.local_addr() {
-        Ok(addr) => Some(addr.ip()),
-        Err(_) => None,
-    }
+pub fn get_local_addr() -> Result<IpAddr, local_ip_address::Error> {
+    local_ip()
 }
 
 #[must_use]

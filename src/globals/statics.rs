84086a68729dc3e93c556eddffdfc9fdb3b3b58a
dheijl <danny.heijl@telenet.be>
2025-02-10 20:42:10 +0100

    Update statics.rs

--- src/globals/statics.rs
+++ src/globals/statics.rs
@@ -18,33 +18,32 @@ pub const SERVER_PORT: u16 = 5901;
 // streaming clients of the webserver
 pub static CLIENTS: LazyLock<RwLock<HashMap<String, ChannelStream>>> =
     LazyLock::new(|| RwLock::new(HashMap::new()));
-// the global GUI logger textbox channel used by all threads
-pub static MSGCHANNEL: LazyLock<RwLock<(Sender<MessageType>, Receiver<MessageType>)>> =
-    LazyLock::new(|| RwLock::new(unbounded()));
-// the global configuration state
-pub static CONFIG: LazyLock<RwLock<Configuration>> =
-    LazyLock::new(|| RwLock::new(Configuration::read_config()));
-// the list of known fltk theme naes
-pub static THEMES: [&str; 6] = ["Shake", "Gray", "Tan", "Dark", "Black", "None"];
-// the global "enable rms monitor" flag
-pub static RUN_RMS_MONITOR: AtomicBool = AtomicBool::new(false);
-
 pub fn get_clients() -> RwLockReadGuard<'static, HashMap<String, ChannelStream>> {
     CLIENTS.read().expect("CLIENTS read lock poisoned")
 }
-
 pub fn get_clients_mut() -> RwLockWriteGuard<'static, HashMap<String, ChannelStream>> {
     CLIENTS.write().expect("CLIENTS write lock poisoned")
 }
 
+// the global GUI logger textbox channel used by all threads
+pub static MSGCHANNEL: LazyLock<RwLock<(Sender<MessageType>, Receiver<MessageType>)>> =
+    LazyLock::new(|| RwLock::new(unbounded()));
+pub fn get_msgchannel() -> RwLockReadGuard<'static, (Sender<MessageType>, Receiver<MessageType>)> {
+    MSGCHANNEL.read().expect("MSGCHANNEL read lock poisoned")
+}
+
+// the global configuration state
+pub static CONFIG: LazyLock<RwLock<Configuration>> =
+    LazyLock::new(|| RwLock::new(Configuration::read_config()));
 pub fn get_config() -> RwLockReadGuard<'static, Configuration> {
     CONFIG.read().expect("CONFIG read lock poisoned")
 }
-
 pub fn get_config_mut() -> RwLockWriteGuard<'static, Configuration> {
     CONFIG.write().expect("CONFIG write lock poisoned")
 }
 
-pub fn get_msgchannel() -> RwLockReadGuard<'static, (Sender<MessageType>, Receiver<MessageType>)> {
-    MSGCHANNEL.read().expect("MSGCHANNEL read lock poisonned")
-}
+// the list of known fltk theme naes
+pub static THEMES: [&str; 6] = ["Shake", "Gray", "Tan", "Dark", "Black", "None"];
+
+// the global "enable rms monitor" flag
+pub static RUN_RMS_MONITOR: AtomicBool = AtomicBool::new(false);

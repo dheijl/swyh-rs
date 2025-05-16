13b1d25f43af726a0800a61006d8b3073413b331
dheijl <danny.heijl@telenet.be>
2025-05-16 12:08:04 +0200

    update windows

--- src/enums/messages.rs
+++ src/enums/messages.rs
@@ -1,7 +1,7 @@
 use crate::{openhome::rendercontrol::Renderer, server::streaming_server::StreamerFeedBack};
 #[derive(Debug, Clone)]
 pub enum MessageType {
-    SsdpMessage(Box<Renderer>),
+    SsdpMessage(Box<Renderer>), // boxed to reduce enum size
     PlayerMessage(StreamerFeedBack),
     LogMessage(String),
 }

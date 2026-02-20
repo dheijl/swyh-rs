43a47e96f75ef31cb2cba252d313e79f117aea2e
dheijl <danny.heijl@telenet.be>
2026-02-20 16:48:06 +0100

    replace libc with rustix
    no more unsafe, no more problem with nix pinning libc.

--- src/utils/priority.rs
+++ src/utils/priority.rs
@@ -31,12 +31,10 @@ pub fn raise_priority() {
     // the following only works when you're root on Linux
     // or if you give the program CAP_SYS_NICE (cf. setcap)
     // or are a user of the pipewire group
-    use libc::{PRIO_PROCESS, getpriority, setpriority};
-    unsafe {
-        let pri = getpriority(PRIO_PROCESS, 0);
+    use rustix::process::{getpriority_process, setpriority_process};
+    if let Ok(pri) = getpriority_process(None) {
         if pri >= 0 {
-            let rc = setpriority(PRIO_PROCESS, 0, -10);
-            if rc != 0 {
+            if setpriority_process(None, -10).is_err() {
                 ui_log(
                     LogCategory::Warning,
                     "Sorry, but you don't have permissions to raise priority...",
